32cf5c986cbdd23ebb75b6234ffcb5edb4a7facf
zjj <zhang799183463@gmail.com>
2020-07-08 11:05:36 +0800

    测试

---
+++ XiaoMaiVideo/app/src/main/java/com/edu/whu/xiaomaivideo/ui/dashboard/DashboardModel.java
@@ -0,0 +1,17 @@
+package com.edu.whu.xiaomaivideo.ui.dashboard;
+
+public class DashboardModel {
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public DashboardModel(String msg) {
+        this.msg = msg;
+    }
+
+    public void setMsg(String msg){
+        this.msg = msg;
+    }
+}
