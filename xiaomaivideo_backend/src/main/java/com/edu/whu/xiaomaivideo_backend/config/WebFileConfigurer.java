73c540b41e0364189b6ee4b5a28155e11dca8c18
zjj <zhang799183463@gmail.com>
2020-07-10 20:39:23 +0800

    后端文件接口

---
+++ xiaomaivideo_backend/src/main/java/com/edu/whu/xiaomaivideo_backend/config/WebFileConfigurer.java
@@ -0,0 +1,17 @@
+package com.edu.whu.xiaomaivideo_backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootConfiguration
+public class WebFileConfigurer implements WebMvcConfigurer {
+    @Value("${constant.pic-file-resource-path}")
+    private String path;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/api/file/**").addResourceLocations("file:" + path);
+    }
+}
