c53a22e7bd4ada7f1ab54da13b61e3fe7b2448df
Ye_Junhao <HuashiSCNU0303@hotmail.com>
2020-07-11 12:04:12 +0800

    yjh_0711_登录功能细化，UI界面调整

--- XiaoMaiVideo/app/src/main/java/com/edu/whu/xiaomaivideo/util/Constant.java
+++ XiaoMaiVideo/app/src/main/java/com/edu/whu/xiaomaivideo/util/Constant.java
@@ -6,7 +6,7 @@ public class Constant {
     // 云服务器URL
     public static final String BASEURL = "http://139.224.133.166:8088/rest/";
 
-    public static User CurrentUser = new User(0); // UserId = 0，表示尚未登录
+    public static User CurrentUser = User.Visitor(); // 尚未登录
 
     public static final int RESULT_SUCCESS = 200;
     public static final int RESULT_FAILURE = 400;
@@ -15,4 +15,6 @@ public class Constant {
     public static final int PASSWORD_WRONG = 2;
     public static final int LOGIN_SUCCESS_RESULT = 3;
     public static final int USER_ALREADY_EXISTS = 4;
+
+    public static final int SETTING_ITEM_COUNT = 1;
 }
