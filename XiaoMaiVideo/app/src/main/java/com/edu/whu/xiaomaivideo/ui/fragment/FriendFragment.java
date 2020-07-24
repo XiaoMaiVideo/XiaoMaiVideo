6e19b2013e5b87996b007239a10b508583fc3437
Plum-SeasonE <781500109@qq.com>
2020-07-24 16:19:43 +0800

    刷新功能准备

--- XiaoMaiVideo/app/src/main/java/com/edu/whu/xiaomaivideo/ui/fragment/FriendFragment.java
+++ XiaoMaiVideo/app/src/main/java/com/edu/whu/xiaomaivideo/ui/fragment/FriendFragment.java
@@ -30,6 +30,11 @@ import com.edu.whu.xiaomaivideo.util.Constant;
 import com.edu.whu.xiaomaivideo.viewModel.FriendViewModel;
 import com.edu.whu.xiaomaivideo.widget.MovieRecyclerView;
 import com.jiajie.load.LoadingDialog;
+import com.scwang.smart.refresh.footer.ClassicsFooter;
+import com.scwang.smart.refresh.header.ClassicsHeader;
+import com.scwang.smart.refresh.layout.api.RefreshLayout;
+import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smart.refresh.layout.listener.OnRefreshListener;
 
 
 import java.util.ArrayList;
@@ -42,7 +47,7 @@ import cn.jzvd.JzvdStd;
 /**
  * Author: 李季东、付浩、李季东
  * Create Time: 2020/7/8
- * Update Time: 2020/7/23
+ * Update Time: 2020/7/24
  * 视频详情页面
  */
 public class FriendFragment extends Fragment {
@@ -76,6 +81,21 @@ public class FriendFragment extends Fragment {
             movieList = new ArrayList<Movie>();
         }
 
+        //刷新
+        friendViewModel.refreshLayout.setRefreshHeader(new ClassicsHeader(this.getContext()));
+        friendViewModel.refreshLayout.setRefreshFooter(new ClassicsFooter(this.getContext()));
+        friendViewModel.refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshlayout) {
+                refreshlayout.finishRefresh(2000/*,false*/);//传入false表示刷新失败
+            }
+        });
+        friendViewModel.refreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(RefreshLayout refreshlayout) {
+                refreshlayout.finishLoadMore(2000/*,false*/);//传入false表示加载失败
+            }
+        });
         return fragmentFriendBinding.getRoot();
     }
 
